83b4cab61dc989cecf655bb1bfc184c5f8674862
Nathanael Spear <nathanaelaspear@gmail.com>
2025-08-19 11:31:27 -0400

    Organized file structure. Created Task Master Project

--- routes/projects.py
+++ routes/projects.py
@@ -24,6 +24,31 @@ def project1_page():
 
     else:
         tasks = TodoList.query.order_by(TodoList.date_created).all()
-        return render_template('projects/project1.html', active_page = 'project1', tasks = tasks)
+        return render_template('projects/project1/project1.html', active_page = 'project1', tasks = tasks)
+    
+@projects_bp.route('/project1/delete/<int:id>')
+def delete(id):
+    task_to_delete = TodoList.query.get_or_404(id)
 
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect(url_for('projects.project1_page'))
+    except:
+        return 'There was a problem deleting that task'
+    
+@projects_bp.route('/project1/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = TodoList.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect(url_for('projects.project1_page'))
+        except:
+            return "There was an issue updating your task"
+        
+    else:
+        return render_template('projects/project1/update.html', active_page = 'project1', task=task)
     
